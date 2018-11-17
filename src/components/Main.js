692a480ead3aa27ea123ffb9d3099251048acbf2
Michael Asimakopoulos <m.asimakopoulos@gmail.com>
2018-11-17 22:25:12 +0200

    Add recaptcha

--- src/components/Main.js
+++ src/components/Main.js
@@ -55,6 +55,7 @@ class Main extends React.Component {
               <label htmlFor="message">Message</label>
               <textarea name="message" id="message" rows="4"></textarea>
             </div>
+            <div data-netlify-recaptcha></div>
             <ul className="actions">
               <li><input type="submit" value="Send Message" className="special" /></li>
               <li><input type="reset" value="Reset" /></li>
